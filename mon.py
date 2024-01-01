e16f5b577ffe7e62a8a9fd19bf22633c64718dc2
tomo1123 <tomocruise01@gmail.com>
2024-01-01 20:43:52 +0900

    monデータベース

--- mon.py
+++ mon.py
@@ -1,2 +1,29 @@
 import datetime
-import pymongo
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['test_database']
+
+stack1 = {
+  'name': 'customer1',
+  'pip': ['python', 'java', 'go'],
+  'into': {'os': 'mac'},
+  'data': datetime.datetime.utcnow()
+}
+
+stack2 = {
+  'name': 'customer2',
+  'pip': ['python', 'java'],
+  'into': {'os': 'windows'},
+  'data': datetime.datetime.utcnow()
+}
+
+db_stacks = db.stacks
+stack_id = db_stacks.insert_one(stack1).inserted_id
+print(stack_id, type(stack_id))
+print("##############################")
+print(db_stacks.find_one({'_id': stack_id}))
+
+
+# from bson.objectid import ObjectId
+
