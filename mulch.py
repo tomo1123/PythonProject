1b53a8591b96505c54af56dc50ccd07936e1808c
tomo1123 <tomocruise01@gmail.com>
2024-01-08 21:38:19 +0900

    threading

--- mulch.py
+++ mulch.py
@@ -10,20 +10,21 @@ logging.basicConfig(
 def worker1(d):
   logging.debug('start')
   i = d['X']
+  d['X'] = i + 1
+  logging.debug(d)
   logging.debug('end')
  
-def worker2():
+def worker2(d):
   logging.debug('start')
+  i = d['X']
+  d['X'] = i + 1
+  logging.debug(d)
   logging.debug('end')
   
 if __name__ == '__main__':
-
-    for _ in range(5):
-        t = threading.Thread(target=worker1)
-        t.daemon = True
-        t.start()
-    for thread in threading.enumerate():
-        if thread is threading.current_thread():
-          print(thread)
-          continue
-        thread.join()
+    d = {'X': 0}
+    t1 =  threading.Thread(target=worker1, args=(d, ))
+    t2 =  threading.Thread(target=worker2, args=(d, ))
+    t1.start()
+    t2.start()
+    
