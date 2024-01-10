a3e2743e3b3554d8f3bff11d757018e01d159b52
tomo1123 <tomocruise01@gmail.com>
2024-01-10 19:34:56 +0900

    doctest

---
+++ docktest.py
@@ -0,0 +1,15 @@
+class Cal(object):
+    def add_num_and_double(self, x, y):
+        """Add and double
+        
+        >>> c = Cal()
+        >>> c.add_num_and_double(1, 1)
+        5
+        """
+        result = x + y
+        result *= 2
+        return result
+      
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
