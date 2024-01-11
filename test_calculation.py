2125df944e26b288676df05e92fe9e8dd5987193
tomo1123 <tomocruise01@gmail.com>
2024-01-11 21:00:04 +0900

    Update test_calculation.py

--- test_calculation.py
+++ test_calculation.py
@@ -7,5 +7,10 @@ class CalTest(unittest.TestCase):
         cal = calculation.Cal()
         self.assertEqual(cal.add_num_and_double(1, 1), 4)
         
+    def test_add_num_and_double_raise(self):
+        cal = calculation.Cal()
+        with self.assertRaises(ValueError):
+            cal.add_num_and_double('1', '1')
+        
 if __name__ == '__main__':
     unittest.main()
