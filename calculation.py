f2ce9be1e40dacef202235fb99575330b252460c
tomo1123 <tomocruise01@gmail.com>
2024-01-13 10:56:10 +0900

    unittest

--- calculation.py
+++ calculation.py
@@ -1,9 +1,14 @@
 class Cal(object):
     def add_num_and_double(self, x, y):
+        """
+        >>> cal = Cal()
+        >>> cal.add_num_and_double(1, 1)
+        4
+        """
         result = x + y
         result *= 2
         return result
-      
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
--- test_calculation.py
+++ test_calculation.py
@@ -2,7 +2,19 @@ import unittest
 
 import calculation
 
+release_name = 'lesson2'
+
 class CalTest(unittest.TestCase):
+    def setUp(self):
+        print('setup')
+        self.cal = calculation.Cal()
+        
+    def tearDown(self):
+        print('clean up')
+        del self.cal
+    
+    # @unittest.skip('skip!')
+    @unittest.skipIf(release_name=='lesson2', 'skip!')
     def test_add_num_and_double(self):
         cal = calculation.Cal()
         self.assertEqual(cal.add_num_and_double(1, 1), 4)
