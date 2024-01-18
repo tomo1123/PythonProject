40c989b06fc5a4c74d6b7e982ed4442eec5b9fd3
tomo1123 <tomocruise01@gmail.com>
2024-01-18 21:21:54 +0900

    pass_word

--- passwrod.py
+++ passwrod.py
@@ -8,18 +8,25 @@ db = {}
 
 salt = base64.b64encode(os.urandom(32))
 
-def get_digest(password):
-  password = bytes(password, 'utf-8')
-  digest = hashlib.sha256(salt + password).hexdigest()
-  for i in range(10000):
-    digest = hashlib.sha256(bytes(digest, 'utf-8')).hexdigest()
-    print(digest)
-  print(digest)
-  return digest
+# def get_digest(password):
+#   password = bytes(password, 'utf-8')
+#   digest = hashlib.sha256(salt + password).hexdigest()
+#   for i in range(10000):
+#     digest = hashlib.sha256(bytes(digest, 'utf-8')).hexdigest()
+#     print(digest)
+#   print(digest)
+#   return digest
+
+digest = hashlib.pbkdf2_hmac(
+  'sha256', bytes(user_pass, 'utf-8'), salt, 10000)
+
+db[user_name] = digest
   
-db[user_name] = get_digest(user_pass)
+# db[user_name] = get_digest(user_pass)
 
 def is_login(user_name, user_pass):
-  return get_digest(user_pass) == db[user_name]
+  digest = hashlib.pbkdf2_hmac(
+    'sha256', bytes(user_pass, 'utf-8'), salt, 10000)
+  return digest == db[user_name]
   
 print(is_login(user_name, 'password'))
